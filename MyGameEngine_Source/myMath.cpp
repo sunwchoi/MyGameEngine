a87f1c87e402eb9cff47ec5c38b03e920503b392
sunwchoi <chltjsdn103@naver.com>
2025-05-22 14:01:05 +0900

    Feat: 3차원 벡터 클래스 추가
    1. 3차원 벡터 클래스 추가
    2. 벡터 멤버 함수 및 math 네임스페이스 함수 추가

--- MyGameEngine_Source/myMath.cpp
+++ MyGameEngine_Source/myMath.cpp
@@ -1,4 +1,6 @@
 #include "myMath.h"
+#include "myAssert.h"
+#include <cmath>
 
 namespace my
 {
@@ -19,6 +21,8 @@ namespace my
 
 	Vector2 Vector2::operator/(float value) const
 	{
+		MY_ASSERT_MSG(value != 0, "Division By Zero");
+
 		return Vector2(_x / value, _y / value);
 	}
 
@@ -48,6 +52,8 @@ namespace my
 
 	Vector2& Vector2::operator/=(float value)
 	{
+		MY_ASSERT_MSG(value != 0, "Division By Zero");
+
 		_x /= value;
 		_y /= value;
 
@@ -57,5 +63,145 @@ namespace my
 	Vector2 operator*(float value, Vector2 rhs)
 	{
 		return rhs * value;
+	}
+
+
+
+/***Vector3**************************************************************************/
+	
+	Vector3 Vector3::operator+(const Vector3& other) const
+	{
+		return Vector3(_x + other._x, _y + other._y, _z + other._z);
+	}
+
+	Vector3 Vector3::operator-(const Vector3& other) const
+	{
+		return Vector3(_x - other._x, _y - other._y, _z - other._z);
+	}
+
+	Vector3 Vector3::operator*(float value) const
+	{
+		return Vector3(_x * value, _y * value, _z * value);
+	}
+
+	Vector3 Vector3::operator/(float value) const
+	{
+		MY_ASSERT_MSG(value != 0, "Division By Zero");
+		return Vector3(_x / value, _y / value, _z / value);
+	}
+
+	Vector3& Vector3::operator+=(const Vector3& other)
+	{
+		_x += other._x;
+		_y += other._y;
+		_z += other._z;
+
+		return *this;
+	}
+
+	Vector3& Vector3::operator-=(const Vector3& other)
+	{
+		_x -= other._x;
+		_y -= other._y;
+		_z -= other._z;
+
+		return *this;
+	}
+
+	Vector3& Vector3::operator*=(float value)
+	{
+		_x *= value;
+		_y *= value;
+		_z *= value;
+
+		return *this;
+	}
+
+	Vector3& Vector3::operator/=(float value)
+	{
+		MY_ASSERT_MSG(value != 0, "Division By Zero");
+
+		_x /= value;
+		_y /= value;
+		_z /= value;
+
+		return *this;
+	}
+
+	bool Vector3::operator==(const Vector3& other) const
+	{
+		return (_x == other._x && _y == other._y && _z == other._z);
+	}
+
+	bool Vector3::operator!=(const Vector3& other) const
+	{
+		return !(*this == other);
+	}
+
+	Vector3 Vector3::Cross(const Vector3& other)
+	{
+		return Vector3(
+			_y * other._z - _z * other._y,
+			_z * other._x - _x * other._z,
+			_x * other._y - _y * other._x
+		);
+	}
+
+	float Vector3::Dot(const Vector3& other)
+	{
+		return _x * other._x + _y * other._y + _z * other._z;
+	}
+
+	Vector3 Vector3::Normalize() const
+	{
+		float size = Size();
+
+		return Vector3(_x / size, _y / size, _z / size);
+	}
+
+	float Vector3::Size() const
+	{
+		return sqrt(_x * _x + _y * _y + _z * _z);
+	}
+
+	Vector3 operator*(float value, Vector3 rhs)
+	{
+		return Vector3(value * rhs._x, value * rhs._y, value * rhs._z);
+	}
+
+	namespace math
+	{
+		Vector3 Cross(const Vector3& a, const Vector3& b) {
+
+			return Vector3(
+				a._y * b._z - a._z * b._y,
+				a._z * b._x - a._x * b._z,
+				a._x * b._y - a._y * b._x
+			);
+		}
+
+		float Dot(const Vector3& a, const Vector3& b) {
+			return a._x * b._x + a._y * b._y + a._z * b._z;
+		}
+
+		Vector3 Normalize(const Vector3& v)
+		{
+			float magnitude = std::sqrt(v._x * v._x + v._y * v._y + v._z * v._z);
+
+			return Vector3(
+				v._x / magnitude,
+				v._y / magnitude,
+				v._z / magnitude
+			);
+		}
+
+		Vector3 GetNormal(const Vector3& a, const Vector3& b, const Vector3& c)
+		{
+			Vector3 edge1 = b - a;
+			Vector3 edge2 = c - a;
+
+			return (edge1.Cross(edge2)).Normalize();
+		}
+
 	}
 }
---
+++ MyGameEngine_Source/myMesh.cpp
@@ -0,0 +1,131 @@
+#include "myMesh.h"
+#include "myResources.h"
+
+namespace my
+{
+    Mesh::Mesh(const std::wstring& name)
+        : Resource(name, eResourceType::Mesh)
+    {
+    }
+
+    Mesh::~Mesh()
+    {
+    }
+
+    void Mesh::Load(const std::wstring& path)
+    {
+        const std::wstring ext = path.substr(path.find_last_of(L".") + 1);
+        MY_ASSERT_MSG(ext == L"obj", ".obj이외의 파일은 아직 기능 없음");
+
+        if (ext == L"obj")
+        {
+            //obj파일 파싱
+            std::string pathStr;
+            pathStr.assign(path.begin(), path.end());
+
+            FILE* file = nullptr;
+
+            fopen_s(&file, pathStr.c_str(), "r");
+            MY_ASSERT_MSG(file, ".obj파일 open 실패");
+
+            vector<Vector3> vertices;
+            vector<uint32> indices;
+            char mtlPath[1024];
+
+            char id[1024];
+            while (!feof(file)) {
+
+                fscanf_s(file, "%s", id, sizeof(id));
+                if (id[0] == 'v' && !id[1])
+                    parseVertex(file, vertices);
+                else if (id[0] == 'f' && !id[1])
+                    parseIndex(file, indices);
+                else if (std::string(id) == "mtllib")
+                    fscanf_s(file, "%s", mtlPath, sizeof(mtlPath));
+            }
+            fclose(file);
+            //
+
+            //메시 데이터로 재입력
+            for (int i = 0; i < indices.size(); i += 3) {
+                Vector3 v1 = vertices[indices[i]],
+                    v2 = vertices[indices[i + 1]],
+                    v3 = vertices[indices[i + 2]];
+
+                Vector3 norm = math::GetNormal(v1, v2, v3);
+
+                _vertices.push_back(Vertex(v1, norm));
+                _vertices.push_back(Vertex(v2, norm));
+                _vertices.push_back(Vertex(v3, norm));
+            }
+
+            //mtl 파일 로드
+            std::string mtlPathStr(mtlPath);
+            std::wstring resourceDir = L"..\\Resource\\" + std::wstring(mtlPathStr.begin(), mtlPathStr.end());
+            
+            _material = Resources::Load<Material>(getName() + L"Material", resourceDir);
+        }
+    }
+
+    void Mesh::parseVertex(FILE* file, vector<Vector3>& vertices)
+    {
+        float x, y, z;
+
+        fscanf_s(file, "%f %f %f", &x, &y, &z);
+        vertices.push_back(Vector3(x, y, z));
+    }
+
+    void Mesh::parseIndex(FILE* file, vector<uint32>& indices)
+    {
+        vector<unsigned int> index;
+        char buffer[1024] = { 0, };
+
+        fgets(buffer, 1024, file);
+        char* context = nullptr;
+        for (char* token = strtok_s(buffer, " ", &context); token; token = strtok_s(nullptr, " ", &context)) {
+            index.push_back(std::stol(token) - 1);
+        }
+
+        if (index.size() == 3) {
+            indices.push_back(index[0]);
+            indices.push_back(index[1]);
+            indices.push_back(index[2]);
+
+        }
+        else if (index.size() == 4) {
+            indices.push_back(index[0]);
+            indices.push_back(index[1]);
+            indices.push_back(index[2]);
+
+            indices.push_back(index[0]);
+            indices.push_back(index[2]);
+            indices.push_back(index[3]);
+        }
+    }
+
+
+    const void* Mesh::getBufferData() const {
+        return _vertices.data();
+    }
+
+    unsigned int Mesh::getBufferStride() const {
+        return sizeof(Vertex);
+    }
+
+    unsigned int Mesh::getBufferSize() const {
+        return _vertices.size() * sizeof(Vertex);
+    }
+
+    void* Mesh::getVertexOffset() const {
+        return (void*)0;
+    }
+
+    void* Mesh::getNormalOffset() const {
+        return (void*)sizeof(Vertex);
+    }
+
+    Material* Mesh::getMaterial() const
+    {
+        return _material;
+    }
+}
