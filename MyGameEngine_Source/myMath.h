6358cb4898f7e829d3847d72039f20a67a2c9b3e
sunwchoi <chltjsdn103@naver.com>
2025-05-22 15:12:36 +0900

    Feat: 누락된 Math 헤더 추가

--- MyGameEngine_Source/myMath.h
+++ MyGameEngine_Source/myMath.h
@@ -25,5 +25,49 @@ namespace my
 		friend Vector2 operator*(float value, Vector2 rhs);
 	};
 
+	class Vector3
+	{
+	public:
+		Vector3() : _x(0.f), _y(0.f), _z(0.f) {}
+		Vector3(float x, float y, float z) : _x(0.f), _y(0.f), _z(0.f) {}
+		~Vector3() {}
+
+		Vector3 operator+(const Vector3& other) const;
+		Vector3 operator-(const Vector3& other) const;
+		Vector3 operator*(float value) const;
+		Vector3 operator/(float value) const;
+
+		Vector3& operator+=(const Vector3& other);
+		Vector3& operator-=(const Vector3& other);
+		Vector3& operator*=(float value);
+		Vector3& operator/=(float value);
+
+		bool operator==(const Vector3& other) const;
+
+		bool operator!=(const Vector3& other) const;
+
+		Vector3 Cross(const Vector3& other);
+
+		float Dot(const Vector3& other);
+
+		Vector3 Normalize() const;
+
+		float Size() const;
+
+		float	_x;
+		float	_y;
+		float	_z;
+	};
+
+	namespace math
+	{
+		float Dot(const Vector3& a, const Vector3& b);
+		Vector3 Cross(const Vector3& a, const Vector3& b);
+		Vector3 Normalize(const Vector3& v);
+		Vector3 GetNormal(const Vector3& a, const Vector3& b, const Vector3& c);
+
+	}
+
+
 
 }
